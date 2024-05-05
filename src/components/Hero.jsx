ae3fad95f721759743b09a49aa3a3a642613fb4d
Manav Rathod <manavrathod203@gmail.com>
2024-05-05 17:43:05 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ function Hero() {
           vel.
         </p>
         <div className="flex items-center flex-wrap gap-4 mt-8">
-          <a href="projects" className="text-[#252422] uppercase whitespace-nowrap text-xl border-2 border-[#252422] rounded-full px-5 py-2 tracking-wide hover:bg-[#252422] hover:text-[#E8E8E3] transition-all ease-in-out duration-150">Projects</a>
+          <a href="#projects" className="text-[#252422] uppercase whitespace-nowrap text-xl border-2 border-[#252422] rounded-full px-5 py-2 tracking-wide hover:bg-[#252422] hover:text-[#E8E8E3] transition-all ease-in-out duration-150">Projects</a>
           <a href="https://drive.google.com/file/d/1Ng7AQj0cxVbB4XY_7poSPdq7vYGgFMO3/view?usp=sharing" target="_blank" className="text-[#252422] uppercase whitespace-nowrap text-xl border-2 border-[#252422] rounded-full px-5 py-2 tracking-wide flex items-center justify-center gap-2 hover:bg-[#252422] hover:text-[#E8E8E3] transition-all ease-in-out duration-150">Download resume <IoArrowUp className="rotate-[45deg]"/></a>
         </div>
       </div>
