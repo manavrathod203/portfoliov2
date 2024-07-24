c70daa20b0b145803be739e13959e68a115cfb99
Manav Rathod <manavrathod203@gmail.com>
2024-07-24 19:59:01 +0530

    resume changes

--- src/data/Links.js
+++ src/data/Links.js
@@ -1,5 +1,5 @@
 const links = {
-    resume:'https://drive.google.com/file/d/1QobW4tpMQ_sJCbGwiZ6LTLV5wG-j1_ml/view?usp=sharing',
+    resume:'https://drive.google.com/file/d/1tg3_CoD-pXex0j_VCsqGsWqNi92zmowN/view?usp=sharing',
     email:'manavrathod203@gmail.com',
     linkedin:'https://www.linkedin.com/in/manavrathod203/',
     github:'https://github.com/manavrathod203',
