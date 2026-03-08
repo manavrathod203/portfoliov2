998da4db6d9d5c764a43334080a4de57e0545a32
Manav Rathod <manavrathod2803@gmail.com>
2026-03-08 12:22:58 +0530

    links

--- src/data/Links.js
+++ src/data/Links.js
@@ -1,8 +1,8 @@
 const links = {
-    resume:'https://drive.google.com/file/d/1Ay0jnMjNwH_6UACFizdxmViA5JO-rXIi/view?usp=sharing',
+    resume:'https://drive.google.com/drive/folders/1-WyHFycDh0dUaqsXkdUySK1IS0FQ7rXK?usp=sharing',
     email:'manavrathod203@gmail.com',
     linkedin:'https://www.linkedin.com/in/manavrathod203/',
-    github:'https://github.com/manavrathod203',
+    github:'https://github.com/manavrathod03',
     instagram:'https://www.instagram.com/manavrathodd/'
 };
 
